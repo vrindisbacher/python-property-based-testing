593e87fd71f2086ddd2014af4e5049371325f3c7
vrindisbacher <vrindisbacher77@gmail.com>
2023-04-22 08:33:27 -0500

    update test cases

--- pybt/tests/simple_nonclass_test.py
+++ pybt/tests/simple_nonclass_test.py
@@ -7,20 +7,19 @@ def rev(l):
 
 
 class TestRevSimple(TestCase):
-    pybt_small = pybt(max_complex_arg_size=5, max_basic_arg_size=1000)
 
-    @pybt_small
+    @pybt
     def test_rev(self, l: list):
         assert rev(rev(l)) == l
 
-    @pybt_small
+    @pybt
     def test_rev_any(self, l: list[any]):
         assert rev(rev(l)) == l
 
-    @pybt_small
+    @pybt
     def test_rev_dict(self, l: list[dict]):
         assert rev(rev(l)) == l
 
-    @pybt_small
+    @pybt
     def test_rev_dict_no_key(self, l: list[dict[str, any]]):
         assert rev(rev(l)) == l
--- pybt/tests/typegen_test.py
+++ pybt/tests/typegen_test.py
@@ -3,45 +3,43 @@ from pybt.core import pybt
 
 
 class TestTypeGen(TestCase):
-    pybt_small = pybt(max_basic_arg_size=100, max_complex_arg_size=10)
-
-    @pybt_small
+    @pybt
     def test_simple_list(self, l: list):
         assert type(l) == list
 
-    @pybt_small
+    @pybt
     def test_simple_dict(self, d: dict):
         assert type(d) == dict
 
-    @pybt_small
+    @pybt
     def test_simple_int(self, i: int):
         assert type(i) == int
 
-    @pybt_small
+    @pybt
     def test_simple_float(self, f: float):
         assert type(f) == float
 
-    @pybt_small
+    @pybt
     def test_simple_bool(self, b: bool):
         assert type(b) == bool
 
-    @pybt_small
+    @pybt
     def test_simple_string(self, s: str):
         assert type(s) == str
 
-    @pybt_small
+    @pybt
     def test_simple_union(self, s: int | float | str | bool):
         valid = [int, float, str, bool]
         assert type(s) in valid
 
-    @pybt_small
+    @pybt
     def test_list_simple_union(self, l: list[int | float | str | bool]):
         valid = [int, float, str, bool]
         assert type(l) == list
         for el in l:
             assert type(el) in valid
 
-    @pybt_small
+    @pybt
     def test_dict_simple_union(
         self, d: dict[int | float | str | bool, int | float | str | bool]
     ):
@@ -51,7 +49,7 @@ class TestTypeGen(TestCase):
             assert type(key) in valid
             assert type(value) in valid
 
-    @pybt_small
+    @pybt
     def test_nested_list_types(self, l: list[list[int | float | str | bool]]):
         valid = [int, float, str, bool]
         assert type(l) == list
@@ -60,7 +58,7 @@ class TestTypeGen(TestCase):
             for e in el:
                 assert type(e) in valid
 
-    @pybt_small
+    @pybt
     def test_nested_dict_types(
         self,
         d: dict[int | str | bool | float, dict[int | str | bool | float, list[int]]],
